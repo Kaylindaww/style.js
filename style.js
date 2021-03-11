c79f27e6def2f3e18dab15a174ec20f9fcd6bf3a
Kaylindaww <79210939+Kaylindaww@users.noreply.github.com>
2021-03-11 10:13:51 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+function fruits(){
+    var fruitsList=["mangoes","avacadoes","oranges","kiwi"];
+    for(let i=0;i <= fruitsList.length; i++){
+        const element=fruitsList[i];
+        console.log(fruitsList[i]);
+
+    }
+}
+fruits();
